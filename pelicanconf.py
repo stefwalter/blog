c5904dd71c89f0d2e5f078a790b30e5baaef00fa
Stef Walter <stefw@redhat.com>
2014-11-20 08:50:31 +0100

    Include publish configuration

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+THEME = "./themes/svbhack"
+TYPOGRIFY = True
+
+AUTHOR = u'Stef Walter'
+SITENAME = u'Oh, Marmalaade!'
+SITEURL = ''
+TAGLINE = u'Stef Walter'
+DISQUS_SITENAME = 'stefthewalternet'
+
+TIMEZONE = 'Utc'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Cockpit', 'http://cockpit-project.org/'),
+          ('Ohloh', 'https://www.ohloh.net/accounts/115261?ref=detailed'),)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images', 'files']
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
